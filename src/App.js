0b323fda50ab884c45c7d504d6f50336d2d2da8f
Carol Chung <cch5ng@gmail.com>
2020-09-22 12:45:58 -0700

    add state for 3 controls

--- src/App.js
+++ src/App.js
@@ -10,26 +10,27 @@ import './App.css';
 
 function App() {
   const [isDarkModeOn, setIsDarkModeOn] = useState(false);
+  const [countrySearchText, setCountrySearchText] = useState('');
+  const [selectedFilterRegion, setSelectedFilterRegion] = useState('');
 
   return (
     <div className="App">
-      <header>
-        <div className="row">
-          <h1>Where in the world?</h1>
-          <button>Dark Mode</button>
-        </div>
-      </header>
-      <main>
-        <Router>
-          <Switch>
-            <Route path="/:name"><Detail /></Route>
-            <Route path="/"><Home /></Route>
-          </Switch>
-        </Router>
-
-      </main>
+      <Router>
+        <header>
+          <div className="row">
+            <Link to="/"><h1>Where in the world?</h1></Link>
+            <button>Dark Mode</button>
+          </div>
+        </header>
+        <main>
+            <Switch>
+              <Route path="/:name"><Detail /></Route>
+              <Route path="/"><Home /></Route>
+            </Switch>
+        </main>
+      </Router>
       <footer>
-        <h2>Set link to source and link to FEMentor site</h2>
+        <h2>FOOTER: Set link to source and link to FEMentor site</h2>
       </footer>
     </div>
   );
@@ -37,7 +38,6 @@ function App() {
 
 function Detail() {
   let { name } = useParams();
-
   return (
     <div>TODO DETAIL ({name})</div>
   )
