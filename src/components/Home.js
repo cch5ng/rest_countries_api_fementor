336bc2c49fafec5d3e14b227e69fb95a096ad945
Carol Chung <cch5ng@gmail.com>
2020-09-23 16:34:24 -0700

    pass countries props to view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React , {useState, useEffect} from 'react';
 import InputSearch from './InputSearch';
 import FilterControl from './FilterControl';
+import CountryCard from './CountryCard';
 
 const Home = (props) => {
-  const {filterSelectHandler, countrySearchText, searchInputChangeHandler} = props;
+  const {filterSelectHandler, countrySearchText, searchInputChangeHandler,
+    countries} = props;
 
   return (
     <div>
@@ -12,6 +14,10 @@ const Home = (props) => {
         onChangeHandler={value => searchInputChangeHandler(value)}/>
 
       <FilterControl filterSelectHandler={ev => filterSelectHandler(ev)} />
+
+      {countries.map(country => (
+        <CountryCard country={country} key={country.name}/>
+      ))}
     </div>
   )
 }
