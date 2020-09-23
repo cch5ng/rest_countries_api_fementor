81d64096ba39d35c6d6ce8dd2618affc0ebfb36c
Carol Chung <cch5ng@gmail.com>
2020-09-23 13:56:28 -0700

    refactor to use button comp

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {useParams} from "react-router-dom";
+import Button from './Button';
 
 const CountryDetail = () => {
   let { name } = useParams();
