e8300281b38c41dd0bbdea2dc179277347f57158
Carol Chung <cch5ng@gmail.com>
2020-09-23 13:56:08 -0700

    refactor to use button comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,16 @@ import {
   Link,
   useParams
 } from "react-router-dom";
+import Button from './Button';
 
-const Header = () => {
+const Header = (props) => {
+  const {buttonClickHandler} = props;
   return (
     <header>
       <div className="row">
         <Link to="/"><h1>Where in the world?</h1></Link>
-        <button>Dark Mode</button>
+        <Button label="Dark Mode" buttonClickHandler={ev => buttonClickHandler(ev)} 
+          buttonId="btn_style"/>
       </div>
     </header>
   )
