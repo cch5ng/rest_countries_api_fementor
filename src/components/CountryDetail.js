68abbefc63c88e3589ab5d377c41c8c25ef0338f
Carol Chung <cch5ng@gmail.com>
2020-09-23 23:13:37 -0700

    pass countries from root to country detail; make api query for border countries

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -2,54 +2,80 @@ import React, {useState, useEffect} from 'react';
 import {useParams} from "react-router-dom";
 import Button from './Button';
 
-const CountryDetail = () => {
+const CountryDetail = (props) => {
   let { name } = useParams();
-  console.log('name', name)
-  const [countryData, setCountryData] = useState({});
+  const {countries} = props;
+  const [borderCountries, setBorderCountries] = useState([]);
 
   const getPrettyString = (objAr) => {
     if (objAr && objAr.length) {
-      console.log('objAr', objAr)
       let names = objAr.map(obj => obj.name);
       return names.join(', ');  
     }
     return;
   }
 
+  const getQuerySuffix = (countriesAr) => {
+    if (countriesAr && countriesAr.length) {
+      return countriesAr.join(';');  
+    }
+    return '';
+  }
+
+  const getCountriesDict = (countries) => {
+    let dict = countries.reduce((accum, cur) => {
+      let name = cur.name;
+      accum[name] = cur;
+      return accum;
+    }, {});
+    return dict;
+  }
+
+  const getCurrentCountry = (countriesDict, name) => {
+    return countriesDict[name];
+  }
+
+  let countriesDict = getCountriesDict(countries);
+  console.log('countriesDict', countriesDict)
+  let country = getCurrentCountry(countriesDict, name);
+
   useEffect(() => {
-    let url = `https://restcountries.eu/rest/v2/name/${name}`;
-    fetch(url)
-      .then(resp => resp.json())
-      .then(json => {
-        if (json.length) {
-          console.log('json[0]', json[0])
-          setCountryData(json[0]);
-        }
-      })
-      .catch(err => console.error('error', err));
-  }, [name])
+    const url = `https://restcountries.eu/rest/v2/alpha?codes=`;
+    let suffix = getQuerySuffix(country.borders);
+
+    if (suffix.length) {
+      fetch(`${url}${suffix}`)
+        .then(resp => resp.json())
+        .then(json => {
+          if (json.length) {
+            setBorderCountries(json);
+          }
+        })
+        .catch(err => console.error('error', err));
+    }
+  }, [country])
 
   return (
     <div>
-      <div><img alt="flag" src={countryData.flag} /></div>
+      <div><img alt="flag" src={country.flag} /></div>
       <div>{name}</div>
       <div>
         <div>
-          <div>Native Name: {countryData.nativeName}</div>
-          <div>Population: {countryData.population}</div>
-          <div>Region: {countryData.region}</div>
-          <div>Sub Region: {countryData.subregion}</div>
-          <div>Capital: {countryData.capital}</div>
+          <div>Native Name: {country.nativeName}</div>
+          <div>Population: {country.population}</div>
+          <div>Region: {country.region}</div>
+          <div>Sub Region: {country.subregion}</div>
+          <div>Capital: {country.capital}</div>
         </div>
 
         <div>
-          <div>Top Level Domain: {countryData.topLevelDomain}</div>
-          <div>Currencies: {getPrettyString(countryData.currencies)}</div>
-          <div>Languages: {getPrettyString(countryData.languages)}</div>
+          <div>Top Level Domain: {country.topLevelDomain}</div>
+          <div>Currencies: {getPrettyString(country.currencies)}</div>
+          <div>Languages: {getPrettyString(country.languages)}</div>
         </div>
       </div>
       <div>
-        Border Countries: {countryData.borders}
+        Border Countries: {country.borders}
       </div>
 
     
