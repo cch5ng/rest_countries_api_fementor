a3a23d176592feeaa145e5ba726250e485a8d6b6
Carol Chung <cch5ng@gmail.com>
2020-09-25 14:06:13 -0700

    start to style header for dark mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,29 @@ import {
   Link,
   useParams
 } from "react-router-dom";
+import classNames from 'classnames/bind';
 import Button from './Button';
+import styles from './Header.module.scss';
+
+let cx = classNames.bind(styles);
 
 const Header = (props) => {
-  const {buttonClickHandler} = props;
+  const {buttonClickHandler, darkMode} = props;
+
+  let headerClassName = cx({
+    light_background_primary: !darkMode,
+    light_typography_primary: !darkMode,
+    dark_background_primary: darkMode,
+    dark_typography_primary: darkMode
+  });
+
   return (
-    <header>
-      <div className="row">
-        <Link to="/"><h1>Where in the world?</h1></Link>
-        <Button label="Dark Mode" buttonClickHandler={ev => buttonClickHandler(ev)} 
-          buttonId="btn_style"/>
+    <header className={headerClassName}>
+      <div >
+        <Link to="/" className={styles.title}>Where in the world?</Link>
       </div>
+      <Button label="Dark Mode" buttonClickHandler={ev => buttonClickHandler(ev)} 
+        buttonId="btn_style"/>
     </header>
   )
 }
