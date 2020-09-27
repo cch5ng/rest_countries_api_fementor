d77fe4effa35ae2b35d393d7b87db876308a3062
Carol Chung <cch5ng@gmail.com>
2020-09-26 17:36:00 -0700

    25 search icon (#27)
    * adjust card in grid; height
    
    * adjust top margin for grid cards
    
    * add search icon and fix color for dark mode
    
    * refactor move some classes to global sheet

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import classNames from 'classnames/bind';
+import {HiSearch} from "react-icons/hi";
 import styles from './InputSearch.module.scss';
+
 
 let cx = classNames.bind(styles);
 
@@ -11,11 +13,22 @@ const InputSearch = (props) => {
     light_background_primary: !darkMode,
     dark_background_primary: darkMode,
     light_typography_primary: !darkMode,
-    dark_typography_primary: darkMode
+    dark_typography_primary: darkMode,
+    light_typography_placeholder: !darkMode,
+    dark_typography_placeholder: darkMode
   });
+
+  let iconClassName = cx({
+    light_icon: !darkMode,
+    dark_icon: darkMode,
+    icon: true
+  })
 
   return (
     <div className={styles.input_container}>
+      <div className={iconClassName}>
+        <HiSearch />
+      </div>
       <input type="text" onChange={ev => onChangeHandler(ev.target.value)} 
         placeholder={placeholder} className={inputClassName} />
     </div>
