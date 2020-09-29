79071360ada80c4e04906c496920f1b3d6f03db4
Carol Chung <cch5ng@gmail.com>
2020-09-28 22:02:43 -0700

    try to test header and resulting style

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,12 @@ test('renders footer text', () => {
   const linkElement = getByText(/project is available here/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders footer text', () => {
+  render(<App />);
+  fireEvent.click(screen.getByRole('button'));
+  const header = getByText(/Where in the world?/i);
+  expect(header).className.toContain('light_background_primary');
+});
+
+
