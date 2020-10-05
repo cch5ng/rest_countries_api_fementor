9f7fa266649d0e454deb049c9e4e13b0d6545d17
Carol Chung <cch5ng@gmail.com>
2020-10-05 12:31:25 -0700

    stub out init alert comp

---
+++ src/components/Alert.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Alert.module.scss';
+import {useDarkMode} from '../context/useDarkMode';
+
+let cx = classNames.bind(styles);
+
+const Alert = (props) => {
+  const {type, message} = props;
+  const {isDarkModeOn} = useDarkMode();
+  const isAlertError = type === 'error';
+
+  let alertClassName = cx({
+    alert_error: isAlertError,
+    alert_success: !isAlertError,
+    alert: true
+  });
+
+  let titleClassName = cx({
+    alert_title_error: isAlertError,
+    alert_title_success: !isAlertError,
+    title: true
+  });
+
+  let messageClassName = cx({
+    alert_message_error: isAlertError,
+    alert_message_success: !isAlertError
+  });
+
+  return (
+    <div className={alertClassName}>
+      {type === 'error' && (
+        <p className={titleClassName}>Error</p>
+      )}
+      {type !== 'error' && (
+        <p className={titleClassName}>Sucess</p>
+      )}
+      <p className={messageClassName}>{message}</p>
+    </div>
+  )
+}
+
+export default Alert;
